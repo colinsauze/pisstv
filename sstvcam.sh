2ad8a23157e7c4f38a84a27edfd4e457deb79de8
Colin Sauze <cos@aber.ac.uk>
2015-12-02 00:13:16 +0000

    script to grab images from a V4L camera

--- sstvcam.sh
+++ sstvcam.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-# sstv camera, Gerrit Polder, PA3BYA
-# shell script to test SSTV camera
-raspistill -t 1 --width 320 --height 256 -e png -o /tmp/image.png
-# add callsign
-mogrify -pointsize 24 -draw "text 10,40 'MYCALL'" /tmp/image.png
-./pisstv /tmp/image.png 22050
-sudo ./pifm_sstv /tmp/image.png.wav
-rm /tmp/image.png*
+
+while [ "0" = "0" ] ; do
+
+    #raspistill -t 1 --width 320 --height 256 -e png -o /tmp/image.png
+    fswebcam --top-banner --title "From: MW0SZE" --font Arial:14 --png 1 -r 320x240 --timestamp "%H:%M %Y-%m-%d"  -D0 /tmp/image.png
+
+    # add callsign
+    #mogrify -pointsize 24 -draw "text 10,40 'MW0SZE'" /tmp/image.png
+
+    killall xli
+    xli -fit -fillscreen -fullscreen /tmp/image.png &
+
+    ./pisstv /tmp/image.png 22050
+    aplay /tmp/image.png.wav
+
+    sleep 30
+
+done
